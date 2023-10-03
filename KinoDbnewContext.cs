92cbd15f96a53f338a4bd3f0e30f24225b1c50f5
Gra-4 <Megalomaniacjester@gmail.com>
2023-10-03 17:01:53 +0300

    1

--- KinoDbnewContext.cs
+++ KinoDbnewContext.cs
@@ -32,6 +32,7 @@ public partial class KinoDbnewContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=(localdb)\\LocalZh;Database=KinoDBNew1_1;Trusted_Connection=True;TrustServerCertificate=True");
+    //=> optionsBuilder.UseSqlServer("Server=DESKTOP-T5P3GVP;Database=KinoDBNew1_1;Trusted_Connection=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
